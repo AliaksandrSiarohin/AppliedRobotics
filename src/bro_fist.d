b62f9a6c8d179dc8897c7bfc0f185509b637f801
ubuntu <ubuntu@ubuntu-VirtualBox.(none)>
2015-11-05 23:33:50 +0100

    Create brofist folder for all files.

--- src/bro_fist.d
+++
@@ -1,2 +0,0 @@
-bro_fist.o: bro_fist.c headers/bro_fist.h headers/bro_comm.h \
- headers/bro_fist.h headers/bro_opts.h headers/bro_bt.h
--- src/bro_opts.d
+++
@@ -1,2 +0,0 @@
-bro_opts.o: bro_opts.c headers/bro_opts.h headers/bro_bt.h \
- headers/bro_fist.h
