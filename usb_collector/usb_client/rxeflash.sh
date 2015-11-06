dc05c5682d55da21a3b293c0df785cb30ed296ea
ubuntu <ubuntu@ubuntu-VirtualBox.(none)>
2015-11-06 19:06:31 +0100

    Remove useless files.

--- usb_collector/usb_client/rxeflash.sh
+++
@@ -1,4 +0,0 @@
-echo Executing NeXTTool to upload usb_client.rxe...
- /home/ubuntu/NXT/nxtOSEK/bin/NeXTTool /COM=usb -download=usb_client.rxe
- /home/ubuntu/NXT/nxtOSEK/bin/NeXTTool /COM=usb -listfiles=usb_client.rxe
-echo NeXTTool is terminated.
--- usb_collector/usb_client_/rxeflash.sh
+++
@@ -1,4 +0,0 @@
-echo Executing NeXTTool to upload USBLoopBack.rxe...
- /home/ubuntu/NXT/nxtOSEK/bin/NeXTTool /COM=usb -download=USBLoopBack.rxe
- /home/ubuntu/NXT/nxtOSEK/bin/NeXTTool /COM=usb -listfiles=USBLoopBack.rxe
-echo NeXTTool is terminated.
