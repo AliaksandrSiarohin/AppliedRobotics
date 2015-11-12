5c0d7ee89f6d002f0b57ac857f47ff26ab47e7fb
pubgin <aliaksandr.siarohin@gmail.com>
2015-11-12 17:30:25 +0100

    Add identification routine.

--- identification/filtering.r
+++ identification/filtering.r
@@ -3,15 +3,17 @@ setwd("~/apr/AppliedRobotics/motor_data");
 #install.packages('signal');
 library(plyr);
 library(signal);
-
+sampling_time = 5;
 filter_data <- function(df, plot = TRUE) {
-  sampling_time = 20;
+  
   df = ddply(df, .(floor(Time / sampling_time)), summarise, Count = Count[1], 
              Time = floor(Time[1]/ sampling_time) * sampling_time);
   n =  length(df$Count);
   speed = (df$Count[2:n] - df$Count[1:(n-1)]) / (df$Time[2:n] - df$Time[1:(n-1)]);
-  #plot(speed, type ='l');
-  b1 <- butter(11, 0.03, type ='low')
+  
+  plot(speed, type = 'l')
+  plot(df$Time[2:n], speed, type ='l');
+  b1 <- butter(1, 0.02, type ='low')
   speed.filtered <- filtfilt(b1, speed);
   
   speed.cutted = speed.filtered[1: (n/2)];
@@ -27,11 +29,11 @@ filter_data <- function(df, plot = TRUE) {
 
 files = c("20_20000.csv", "30_20000.csv", "40_20000.csv", "50_20000.csv", "60_20000.csv",
           "65_20000.csv", "70_20000.csv", "75_20000.csv", "80_20000.csv", "90_20000.csv")
-
+#files = "90_20000.csv"
 for (filename in files) {
   df = read.csv(filename);
   pdf(paste("plots/", filename, ".pdf", sep = ""));
-  result = filter_data(df);
+  result = filter_data(df, plot = TRUE);
   write.csv(file = paste("velocity/", filename, sep=""), quote = FALSE, result, row.names = FALSE);
   dev.off();
 }
